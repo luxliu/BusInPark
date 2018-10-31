40ecc6d49bcd5619e3716346c7f892c6f7af1e7b
LuxLiu <luxliu007168@gmail.com>
2018-10-31 17:56:43 +1300

    Init

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+
+
+const path = require("path");
+
+module.exports = {
+    mode: 'development',
+    entry: "./app/main.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["babel-preset-env"]
+                    }
+                }
+            }
+        ]
+    },
+    devServer: {
+        port: 3000
+    }
+};
